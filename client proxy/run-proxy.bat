b622454954042d71d7325ca1911b668262992545
BrentonHolloway <brenton.holloway@gmail.com>
2019-08-08 14:35:36 +1000

    client proxy for cloud sql

---
+++ client proxy/run-proxy.bat
@@ -0,0 +1,16 @@
+ECHO OFF
+SET /p input="x64 or x86?"
+IF /i "%input%" == "x64" GOTO x64
+IF /i "%input%" == "x86" GOTO x86
+:x64
+CLS
+ECHO "Running x64 version of client proxy"
+cloud_sql_proxy_x64.exe -instances=pap-app-01:australia-southeast1:pap-db=tcp:3306
+GOTO end
+:x86
+CLS
+ECHO "Running x86 version of client proxy"
+cloud_sql_proxy_x86.exe -instances=pap-app-01:australia-southeast1:pap-db=tcp:3306
+:end
+PAUSE
+EXIT
