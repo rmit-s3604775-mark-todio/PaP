e7b48aa4f3610103b2d7ece0cc31beeaf013ccae
BrentonHolloway <brenton.holloway@gmail.com>
2019-08-14 16:24:02 +1000

    Login and Register pages fully functional

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,14 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:128', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'address_line_1' => ['max:128'],
+            'address_line_2' => ['max:128'],
+            'state_province' => ['required', 'string', 'max:128'],
+            'post_code' => ['required', 'numeric'],
+            'country' => ['required', 'string'],
+            'city' => ['required', 'string'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -65,7 +72,14 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
+            'address_line_1' => $data['address_line_1'],
+            'address_line_2' => $data['address_line_2'],
+            'state_province' => $data['state_province'],
+            'post_code' => $data['post_code'],
+            'country' => $data['country'],
+            'city' => $data['city'],
             'password' => Hash::make($data['password']),
         ]);
     }
