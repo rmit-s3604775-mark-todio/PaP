7475491634dc7dd04b17d328e065c9e34edc2446
BrentonHolloway <brenton.holloway@gmail.com>
2019-08-22 15:31:55 +1000

    user logout fix

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -10,7 +10,7 @@ class AdminLoginController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('guest:admin', ['except' => ['logout']]);
+        $this->middleware('guest:admin')->except('logout');
     }
 
     public function showLoginForm()
