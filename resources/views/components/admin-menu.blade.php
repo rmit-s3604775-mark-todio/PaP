14a72442350625a9b1b2b5d0ab241ea77a0ad0ac
BrentonHolloway <brenton.holloway@gmail.com>
2019-08-26 13:10:02 +1000

    admin side menu css complete

--- resources/views/components/admin-menu.blade.php
+++ resources/views/components/admin-menu.blade.php
@@ -1,30 +1,28 @@
-<div class="container">
-    <div class="row profile-image">
+<div class="row profile-image">
+    <img src="/uploads/avatars/default.png" style="width: 90%; height: 90%; float: center; border-radius: 50%; margin: 5px;">
+</div>
+<div class="row main-menu">
+    <div class="col">
+        <a href="{{ route('admin.dashboard') }}" class="row menu-item">
+            {{ __('Home') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Settings') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Users') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Administrators') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{  __('Products') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Inbox') }}
+        </a>
+    </div>
     
-    </div>
-    <div class="row main-menu">
-        <div class="col">
-            <a href="{{ route('admin.dashboard') }}" class="row menu-item">
-                {{ __('Home') }}
-            </a>
-            <a href="" class="row menu-item">
-                {{ __('Settings') }}
-            </a>
-            <a href="" class="row menu-item">
-                {{ __('Users') }}
-            </a>
-            <a href="" class="row menu-item">
-                {{ __('Administrators') }}
-            </a>
-            <a href="" class="row menu-item">
-                {{  __('Products') }}
-            </a>
-            <a href="" class="row menu-item">
-                {{ __('Inbox') }}
-            </a>
-        </div>
-        
-    </div>
 </div>
 
 
