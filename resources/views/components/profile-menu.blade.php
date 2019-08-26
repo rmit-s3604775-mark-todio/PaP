66f6ce12efb4e2914af3efbb395bc0d9bf31c0ad
rmit-s3630023-larry-ho <s3630023@student.rmit.edu.au>
2019-08-26 16:57:32 +1000

    Using a local database

---
+++ resources/views/components/profile-menu.blade.php
@@ -0,0 +1,27 @@
+<div class="row profile-image">
+    <img src="/uploads/avatars/default.png" style="width: 90%; height: 90%; float: center; border-radius: 50%; margin: 5px;">
+</div>
+<div class="row main-menu">
+    <div class="col">
+        <a href="" class="row menu-item">
+            {{ __('Profile') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Products') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Products Request') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Reviews') }}
+        </a>
+        <a href="" class="row menu-item">
+            {{ __('Inbox') }}
+        </a>
+    </div>
+    
+</div>
+
+
+
+
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,67 +3,86 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Test Profile</div>
-					<div class="container">
-						<div class="row">
-							<div class="col-3 nav-menu justify-content-center">
-								@component('components.profile-menu')
-								@endcomponent
-							</div>		
+                <div class="card-header">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <span class="col-6 text-left">
+                                Profile Page
+                            </span>
+                            <span class="col-6 text-right">
+                               
+                            </span>
+                        </div> 
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3 nav-menu justify-content-center">
+                            @component('components.profile-menu')
+                            @endcomponent
+                        </div>
+                        <div class="col-9">
+                            <div class="card-body">
+                                	<?php
 	
-	<?php
-	
-	# session ID
-	
-	define("DB_HOST", "35.189.36.51");
-	define("DB_NAME", "pap_db");
-	define("DB_USER", "root");
-	define("DB_PASS", "7hvwaEG7q0jxi3vt");
-	
-	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	
-	if (!$conn) {
-		die('Could not connect: ' . mysql_error());
-	};
-	
-		# temporary username to query specific record
-		//$username = 'test';
-		
-		//$result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
-		
-		//if(mysqli_num_rows($result) > 0) {
-		//	$row=mysqli_fetch_assoc($result);
-							
-		// profile picture
-		// username, retrieves username depending on id from database
-		// state
-		//echo $row['state'];
-		echo ("Victoria");
-		echo ("<br>");
-		// city
-		//echo $row['city'];
-		echo ("Melbourne");
-		echo ("<br>");
-		// country
-		//echo $row['country'];
-		echo ("Australia");
-		echo ("<br>");
-		// profile blurb
-		echo ("This is my profile!");
-		echo ("<br>");
-		//}
-		//else {
-		//	echo "Profile does not exist";
-		//}
-		
-		mysqli_close($conn);
-		?>
-			</div>
-		</div>
-	</div>
+									# session ID
+									
+									define("DB_HOST", "localhost");
+									define("DB_NAME", "pap_db");
+									define("DB_USER", "root");
+									define("DB_PASS", "");
+									
+									$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+									
+									if (!$conn) {
+										die('Could not connect: ' . mysql_error());
+									};
+									
+										# temporary username to query specific record
+										//$username = 'test';
+										
+										//$result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+										
+										//if(mysqli_num_rows($result) > 0) {
+										//	$row=mysqli_fetch_assoc($result);
+															
+										// profile picture
+										// username, retrieves username depending on id from database
+										// state
+										//echo $row['state'];
+										echo ("Victoria, ");
+										echo ("<br>");
+										// city
+										//echo $row['city'];
+										echo ("Melbourne");
+										echo ("<br>");
+										// country
+										//echo $row['country'];
+										echo ("Australia");
+										echo ("<br>");
+										// profile blurb
+										echo ("This is my profile!");
+										echo ("<br>");
+										//}
+										//else {
+										//	echo "Profile does not exist";
+										//}
+										
+										mysqli_close($conn);
+									?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+
+	
+
 
 
