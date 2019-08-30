51eddd1ae2c107a8c842f75747fb2e79c41f11c2
BrentonHolloway <brenton.holloway@gmail.com>
2019-08-30 10:01:41 +1000

    user homepage, menu and product requests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,9 @@
                                         <a class="dropdown-item" href="{{ route('home') }}">
                                             {{ __('Home') }}
                                         </a>
+                                        <a class="dropdown-item" href="{{ route('product-requests') }}">
+                                            {{ __('Product Requests') }}
+                                        </a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
@@ -104,6 +107,9 @@
                                         </p>
                                         <a class="dropdown-item" href="{{ route('home') }}">
                                             {{ __('Home') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ route('product-requests') }}">
+                                            {{ __('Product Requests') }}
                                         </a>
                                         <a class="dropdown-item" href="{{ route('user.logout') }}">
                                             {{ __('Logout') }}
