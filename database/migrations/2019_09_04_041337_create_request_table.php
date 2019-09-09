364102731660590615dedaf94bf4fef7be014e08
BrentonHolloway <brenton.holloway@gmail.com>
2019-09-09 17:43:32 +1000

    product request db update

--- database/migrations/2019_09_04_041337_create_request_table.php
+++ database/migrations/2019_09_04_041337_create_request_table.php
@@ -18,8 +18,8 @@ class CreateRequestTable extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('product_name');
-            $table->bigInteger('brand_id')->unsigned()->nullable();
-            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->bigInteger('brand')->unsigned()->nullable();
+            $table->foreign('brand')->references('brand')->on('brands');
             $table->string('condition')->nullable();
             $table->foreign('condition')->references('condition')->on('conditions');
             $table->double('min_price', 8, 2)->nullable();
